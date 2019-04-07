fd69d864719e5a0b1b7cf32d0a45bbfaec8902d2
Tomaz Rodrigues <tomaz.r.rodrigues@gmail.com>
2019-04-07 21:21:03 +0000

    Automatically backed up by Learn

--- app/policies/user_policy.rb
+++ app/policies/user_policy.rb
@@ -1,14 +1,3 @@
 class UserPolicy < ApplicationPolicy
-  def index
-    user.admin? || user.mod?
-  end
-  def show
-    rrecord.try(:user) == user || user.admin? || user.mod?
-  end
-  def update
-    record.try(:user) == user || user.admin?
-  end
-  def destroy
-    user.admin? || record.try(:user) == user
-  end
+
 end
