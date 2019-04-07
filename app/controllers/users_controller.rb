72c5807ad920a1c203fe6886da2062882627b689
Tomaz Rodrigues <tomaz.r.rodrigues@gmail.com>
2019-04-07 21:28:19 +0000

    Automatically backed up by Learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,22 @@
 class UsersController < ApplicationController
   def show
+    if user_signed_in? 
+      @user = authorize User.find_by(id: params[:id])
+    end   
   end
   def index
+    if user_signed_in? 
+      @user = authorize current_user
+    end   
   end
   def update
+    if user_signed_in? 
+      @user = authorize current_user
+    end   
   end
   def destroy
   end
+
+
+  
 end         
